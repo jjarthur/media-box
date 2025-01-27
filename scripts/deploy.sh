1a5684f8b40c9bb0d4f1223e2e77991c638a8b5f
jjarthur <jarthur96@hotmail.com>
2025-01-27 14:21:41 +1100

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+script_dir=$(dirname "$(realpath "$0")")
+
+deploy () {
+    scp $compose_path $host:/docker
+
+    ssh $host "cd /docker && docker compose up -d --force-recreate"
+}
+
+case $1 in
+    jellyfin)
+        compose_path="$script_dir/../jellyfin/docker-compose.yml"
+        host=$JELLYFIN
+        ;;
+    media)
+        compose_path="$script_dir/../media/docker-compose.yml"
+        host=$MEDIA
+        ;;
+    nginx)
+        compose_path="$script_dir/../nginx-proxy-manager/docker-compose.yml"
+        host=$NGINX
+        ;;
+    *)
+        echo "Invalid argument"
+        exit 1;;
+esac
+
+deploy
